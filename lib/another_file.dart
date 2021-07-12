c43b5ececef69c0a2224b3a22bedab26cc992d1a
Abdul Ahmed Abdul <34511653+ahmadabdul592@users.noreply.github.com>
2021-07-12 17:34:49 +0100

    Add files via upload

---
+++ lib/another_file.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        primaryColor: Colors.black12,
+        accentColor: Colors.orange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stateless'),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
